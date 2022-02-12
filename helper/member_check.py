6fa407f1b3752a49ee1d34a6f8d9fdf9a810959a
Prakhar Kumar <prakhar@setu.co>
2022-02-12 17:17:17 +0530

    Added checks for already subscribed channels in channel invite channel

---
+++ helper/member_check.py
@@ -0,0 +1,24 @@
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+
+def return_channels_of_user(client, user_id):
+
+    try:
+        next_cursor = "1"
+        channel_ids = []
+        while next_cursor != "":
+            result = client.users_conversations(
+                user=user_id,
+            ).data
+            for channels in result["channels"]:
+                if channels["is_channel"] == True:
+                    channel_ids.append(channels['id'])
+
+            next_cursor = result["response_metadata"]["next_cursor"]
+        return channel_ids
+    except Exception as e:
+        logger.info(e)
+        return []
